05c50bdc1a9bcea459e6fd324e3d20757000d24b
Hugo Carreira <hugo.colombo23@gmail.com>
2018-10-18 08:02:54 -0300

    :white_check_mark: :recycle: Refactor tests

--- decent_copy_test.go
+++ decent_copy_test.go
@@ -9,8 +9,8 @@ import (
 func TestCopy(t *testing.T) {
 	execPath, _ := os.Getwd()
 
-	fileOrigin := "/testCopy.txt"
-	fileDestiny := fmt.Sprintf(execPath + "/folder/" + fileOrigin)
+	fileOrigin := fmt.Sprintf(execPath + "/testCopy.txt")
+	fileDestiny := fmt.Sprintf(execPath + "/folder/testCopy.txt")
 	err := Copy(fileOrigin, fileDestiny)
 	if err != nil {
 		t.Fatalf("Error in copy file : %#v ", err.Error())
