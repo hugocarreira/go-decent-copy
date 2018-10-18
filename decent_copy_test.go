cc91ee31bb162998079558240e98c92a57f31875
Hugo Carreira <hugo.colombo23@gmail.com>
2018-10-18 08:07:35 -0300

    :bug: Alter name for copy file in test

--- decent_copy_test.go
+++ decent_copy_test.go
@@ -10,7 +10,7 @@ func TestCopy(t *testing.T) {
 	execPath, _ := os.Getwd()
 
 	fileOrigin := fmt.Sprintf(execPath + "/testCopy.txt")
-	fileDestiny := fmt.Sprintf(execPath + "/folder/testCopy.txt")
+	fileDestiny := fmt.Sprintf(execPath + "/testCopied.txt")
 	err := Copy(fileOrigin, fileDestiny)
 	if err != nil {
 		t.Fatalf("Error in copy file : %#v ", err.Error())
